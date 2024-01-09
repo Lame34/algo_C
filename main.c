3df56c21e7d15d3d256cc03315ec460c9ff9a18e
LilianC <100050930+Lame34@users.noreply.github.com>
2024-01-09 15:28:08 +0000

    :v 0.0.2 : variables

--- main.c
+++ main.c
@@ -4,8 +4,11 @@ int main()
 {
   int num = 20;
   int num2 = 40;
+  float price = 10.99;
+  char c = 'c';
+
   printf("Hello World %d \n", num2);
 
-  printf("%d ,  %i\n",num, num2);
+  printf("La valeur de num = %d ,  La valeur de num2 = %i ,  Prix = %.2f,  char value = %c \n", num, num2, price, c);
   return 0;
 }
