1a6b6f23b3abcc1034614fbbb0687603c325e509
LilianC <100050930+Lame34@users.noreply.github.com>
2024-01-17 09:32:49 +0000

    :v 0.0.6 : conditions

--- main.c
+++ main.c
@@ -10,7 +10,18 @@ void afficher(int x)
   printf("Le résultat est : %d\n", x);
 }
 
+int ismajor()
+{
+  int age = 1;
+  if (age > 18) {
+    printf("Vous êtes majeur\n");
+  } else {
+    printf("Vous êtes mineur\n");
+  }
+}
+
 int main()
 {
   afficher(calculer_somme(2, 4));
+  ismajor();
 }
