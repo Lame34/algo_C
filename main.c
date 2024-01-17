35a2d06d422511b84624469ba1c9418ee8eb663e
LilianC <100050930+Lame34@users.noreply.github.com>
2024-01-17 11:02:45 +0000

    :v 0.0.7 :  majeur, constantes

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define MAX_AGE 100
+#define MIN_AGE 0
+#define MAJOR 18
+
 int calculer_somme(int x, int y)
 {
   return x + y;
@@ -10,18 +14,51 @@ void afficher(int x)
   printf("Le résultat est : %d\n", x);
 }
 
-int ismajor()
+
+
+
+
+
+
+int est_majeur(int age)
 {
-  int age = 1;
-  if (age > 18) {
-    printf("Vous êtes majeur\n");
-  } else {
-    printf("Vous êtes mineur\n");
-  }
+  if (age >= 18 && age < 100)
+    return 1;
+  else if (age <= 17 && > 0)
+    return 0;
+  else
+    return -1;
 }
+
+
+void afficher_age(int age)
+{
+  if (est_majeur(age) == 1)
+    printf("%d : majeur\n", age);
+  else if (est_majeur(age) == 0)
+    printf("%d : mineur\n", age);
+  else
+  printf("%d : erreur\n", age);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
-  afficher(calculer_somme(2, 4));
-  ismajor();
+  // afficher(calculer_somme(2, 4));
+  // afficher_age(18);
+  // afficher_age(17);
+  // afficher_age(100);
+  // afficher_age(-10);
 }
