9afdebb3e8efe9caf68816907c3593508fad75ca
LilianC <100050930+Lame34@users.noreply.github.com>
2024-01-23 11:27:53 +0000

    :v 0.2.0 : correction

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #define MIN_AGE 0
 #define MAJOR 18
 
-int calculer_somme(int x, int y);
+void afficher_resultat(int x);
 
-void afficher(int x);
+int calculer_somme(int x, int y);
 
 int est_majeur(int age);
 
@@ -24,63 +24,53 @@ int le_plus_petit(int x, int y);
 
 int est_impaire(int x);
 
+int multiple(int x, int y);
+
 int main()
 {
   int a = 11;
   int b = 10;
   int c = 5;
 
-  printf("Le plus grand entre %d et %d est : %d \n", a, c, le_plus_grand);
+  afficher_resultat(calculer_somme(3, 3));
 
-  printf("Le plus petit entre %d et %d est : %d \n", a, c, le_plus_petit);
+  afficher_resultat(calculer_somme(a, b));
+
+  afficher_age(18);
+  afficher_age(17);
+  afficher_age(100);
+  afficher_age(101);
+  afficher_age(-10);
+
+  if (est_plus_grand(a, c) == 1)
+    printf("%d est plus grand que %d\n", a, c);
+
+  if (est_plus_petit(c, b) == 1)
+    printf("%d est plus petit que %d\n", c, a);
+
+  if (est_egal(a, b) == 1)
+    printf("%d est esgal à %d\n", a, b);
+
+  printf("le plus grand entre %d et %d est : %d \n", a, c, le_plus_grand(a, c));
+
+  printf("le plus petit entre %d et %d est : %d \n", a, c, le_plus_petit(a, c));
 
   if (est_impaire(a) == 0)
-    printf("%d est paire \n", a);
+    printf("%d est pair \n", a);
   else
-    printf("%d est impair \n", a);
+    printf("%d est impaire \n", a);
+
+  multiple(2, 58);
 }
 
-int est_plus_grand(int x, int y)
+void afficher_resultat(int x)
 {
-  if (x > y)
-    return 1;
-}
-int est_plus_petit(int x, int y)
-{
-  if (x < y)
-    return 1;
-}
-
-int est_egal(int x, int y)
-{
-  if (est_plus_grand(x, y) != 1 && est_plus_petit(x, y) != 1)
-    return 1;
-}
-
-int le_plus_grand(int x, int y)
-{
-  if (le_plus_grand(x, y) == 1)
-    return x;
-  else
-    return y;
-}
-
-int le_plus_petit(int x, int y)
-{
-  if (est_plus_petit(x, y) == 1)
-    return x;
-  else
-    return y;
+  printf("le résultat est de : %d\n", x);
 }
 
 int calculer_somme(int x, int y)
 {
   return x + y;
-}
-
-void afficher_resultat(int x)
-{
-  printf("Le résultat est : %d\n", x);
 }
 
 int est_majeur(int age)
@@ -100,10 +90,76 @@ void afficher_age(int age)
   else if (est_majeur(age) == 0)
     printf("%d : mineur\n", age);
   else
-    printf("%d : erreur\n", age);
+    printf("%d :erreur\n", age);
+}
+
+int est_plus_grand(int x, int y)
+{
+  if (x > y)
+    return 1;
+}
+
+int est_plus_petit(int x, int y)
+{
+  if (x < y)
+    return 1;
+}
+
+int est_egal(int x, int y)
+{
+  if (est_plus_grand(x, y) != 1 && est_plus_petit(x, y) != 1)
+    return 1;
+}
+
+int le_plus_grand(int x, int y)
+{
+  if (est_plus_grand(x, y) == 1)
+    return x;
+  else
+    return y;
+}
+
+int le_plus_petit(int x, int y)
+{
+  if (est_plus_petit(x, y) == 1)
+    return x;
+  else
+    return y;
 }
 
 int est_impaire(int x)
 {
   return x % 2;
+}
+
+int multiple(int x, int y)
+{
+  int a = 0;
+  while (a <= y)
+  {
+    printf("%d\n", a);
+    a += x;
+  }
+}
+
+int pair(int x)
+{
+  x = 0;
+  while (x <= 58)
+  {
+    printf("%d \n", x);
+    x = x + 2;
+    x++;
+  }
+}
+
+int impair(int x)
+{
+  x = 1;
+  while (x <= 58)
+  {
+    printf("%d \n", x);
+    x = x + 2;
+    x++;
+  }
 }
